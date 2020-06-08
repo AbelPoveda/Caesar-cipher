e331c0894df1b94025653ea930892d0a3d460c0c
Thebuco1701 <Thebuco1701@users.noreply.github.com>
2020-06-08 14:14:37 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        bienvenida();
-    }
-
-    public static void bienvenida() {
-        boolean ok = false;
-        String eleccion;
-        System.out.println("Bienvenido a Caesar cipher!");
-        System.out.println("Pulsa 1 para encriptar un código, 2 para desencriptarlo o 0 para salir");
-        do {
-            Scanner scan = new Scanner(System.in);
-            eleccion = scan.nextLine();
-            if (eleccion.matches("^[0-2]$")){
-                System.out.println("guay " + eleccion);
-                ok = true;
-            } else {
-                System.out.println("Elige entre 0 para salir, 1 para encriptar y 2 para desencriptar");
-            }
-        } while (!ok);
     }
 }
